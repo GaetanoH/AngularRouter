66720461730c1806492f8a2360b5666e2ec19ace
Gaetano Herman <gaetano.herman.u9781@student.hogent.be>
2017-08-23 17:03:55 +0200

    Finished the routing for the app
    Added CanDeactivate, CanActivate and Resolvers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,14 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {RouterModule, Routes} from '@angular/router';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent},
-  ]},
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent},
-    { path: ':id/edit', component: EditServerComponent}
-  ]},
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import {AuthGuardService} from './auth-guard.service';
+import {AuthService} from './auth.service';
+import {CanDeactivateGuard} from './servers/edit-server/can-deactive-guard.service';
+import {CanDeactivate} from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import {ServerResolver} from './servers/server/server-resolver.service';
 
 @NgModule({
   declarations: [
@@ -32,16 +28,17 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes),
-
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthGuardService, AuthService, CanDeactivateGuard, ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
